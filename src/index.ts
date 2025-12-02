2cfd4d2181d61c849db9005d4c929cd84926cd19
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-12-02 21:03:14 +0100

    chore: add user-related route files and integrate routers for jobs, me, and users

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from 'express'
 import cors from 'cors'
 import { CONFIG } from './config/config'
+import { jobsRouter } from './routes/jobs'
+import { meRouter } from './routes/me'
+import { usersRouter } from './routes/users'
 
 
 const app = express()
@@ -9,6 +12,11 @@ app.use(express.json())
 
 app.use(cors())
 
+app.use('/users/jobs', jobsRouter)
+
+app.use('/users/me', meRouter)
+
+app.use('/users', usersRouter)
 
 app.listen(CONFIG.PORT, () => {
     console.log(`Server listening on port: ${CONFIG.PORT}`)
---
+++ src/routes/jobs.ts
@@ -0,0 +1,5 @@
+import express from 'express'
+import type { Router } from 'express'
+
+
+export const jobsRouter: Router = express.Router()
