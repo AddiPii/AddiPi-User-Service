cd297a8d489e951efb9f1123acd61b467ec1d135
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-12-03 16:20:47 +0100

    Update users.ts
    /users endpoint

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,5 +1,8 @@
 import express from 'express'
 import type { Router } from 'express'
+import { getAllUsers } from './controllers/usersController'
 
 
 export const usersRouter: Router = express.Router()
+
+usersRouter.get('/', getAllUsers)
