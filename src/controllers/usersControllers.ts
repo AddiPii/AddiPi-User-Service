c1f3892f4028ac547258ffd6480b10873784c27a
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-12-03 21:16:53 +0100

    Add error handling to getAllUsers function in usersControllers.ts

--- src/controllers/usersControllers.ts
+++ src/controllers/usersControllers.ts
@@ -6,7 +6,12 @@ export const getAllUsers = async (
     req: Request,
     res: Response
 ): Promise<void | Response<{error: string}>>=> {
-    const query = 'SELECT * FROM c'
-    
-    const { resources: users } = await usersContainer.items.query(query)
+    try {
+        const query = 'SELECT * FROM c'
+        
+        const { resources: users } = await usersContainer.items.query(query)
+    } catch (error) {
+        console.error('Get all users error ', error)
+        res.status(500).json({ error: 'Internal server error' })
+    }
 }
