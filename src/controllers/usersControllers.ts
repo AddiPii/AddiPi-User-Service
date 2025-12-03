c186c25e0e405c9d610898a90f823fd52257e075
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-12-03 16:29:17 +0100

    Update usersControllers.ts
    Prepare query and resources for response

--- src/controllers/usersControllers.ts
+++ src/controllers/usersControllers.ts
@@ -6,6 +6,7 @@ export const getAllUsers = async (
     req: Request,
     res: Response
 ): Promise<void | Response<{error: string}>>=> {
+    const query = 'SELECT * FROM c'
     
-
+    const { resources: users } = await usersContainer.items.query(query)
 }
