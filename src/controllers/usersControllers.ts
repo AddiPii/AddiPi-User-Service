e1d763d481ce5d780c7f8221f6a9739f513d0079
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-12-03 16:16:28 +0100

    Update usersControllers.ts
    Prepare to add /users endpoint for admin to get all users

---
+++ src/controllers/usersControllers.ts
@@ -0,0 +1,11 @@
+import { usersContainer, jobsContainer } from './services/containers'
+import type { Request, Response } from 
+
+
+export const getAllUsers = async (
+    req: Request,
+    res: Response
+): Promise<void | Response<{error: string}>>=> {
+    
+
+}
