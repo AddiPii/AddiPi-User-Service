a5e3125ad959db4e15ab449e0b6f9a72fd857180
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-12-04 02:15:24 +0100

    handle role updated on PATCH route

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import type { Router } from 'express'
-import { getAllUsers, getUserById } from '../controllers/usersControllers'
+import { getAllUsers, getUserById, updateUserRole } from '../controllers/usersControllers'
 import requireAuth from '../middleware/requireAuth'
 import requireAdmin from '../middleware/requireAdmin'
 
@@ -10,3 +10,5 @@ export const usersRouter: Router = express.Router()
 usersRouter.get('/', requireAuth, requireAdmin, getAllUsers)
 
 usersRouter.get('/:userId', requireAuth, requireAdmin, getUserById)
+
+usersRouter.patch('/:userId/role', updateUserRole)
