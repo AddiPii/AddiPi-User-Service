82e73eac45912bb2e65ae6908722143417980242
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-12-04 01:30:18 +0100

    Fix sorting logic in getAllUsers function to handle undefined sort parameter

--- src/controllers/usersControllers.ts
+++ src/controllers/usersControllers.ts
@@ -8,11 +8,17 @@ export const getAllUsers = async (
     res: Response<{error: string} | {users: Array<User>, count: number}>
 ): Promise<void | Response<{error: string}>> => {
     try {
-        const { sort }: { sort: string } = req.query
+        const { sort }: { sort: string | undefined } = req.query
 
         const limit: number = Math.min(Math.max(
             parseInt(req.query.limit as string || '50', 10), 1), 100)
-        let query: string = 'SELECT * FROM c ORDER BY c.createdAt ' + sort || 'DESC'
+        let query: string = 'SELECT * FROM c ORDER BY c.createdAt'
+
+        if ( sort === "ASC" || sort === "asc" ){
+            query += " ASC"
+        } else {
+            query += " DESC"
+        }
         
         const { resources: users } = await usersContainer.items.query(query, {
             maxItemCount: limit
