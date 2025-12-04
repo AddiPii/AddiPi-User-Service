b2ef89ea53f2aa84ee712f0c9811d44af0e99d3f
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-12-04 01:31:15 +0100

    Dodano autoryzację i wymagania administracyjne do trasy pobierania użytkowników

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import type { Router } from 'express'
 import { getAllUsers } from '../controllers/usersControllers'
+import requireAuth from '../middleware/requireAuth'
+import requireAdmin from '../middleware/requireAdmin'
 
 
 export const usersRouter: Router = express.Router()
 
-usersRouter.get('/', getAllUsers)
+usersRouter.get('/', requireAuth, requireAdmin, getAllUsers)
