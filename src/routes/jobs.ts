d0d452c55802d2e3ca62588eb47e81033366e793
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-12-05 01:53:04 +0100

    Add missing import and route for upcoming jobs retrieval

--- src/routes/jobs.ts
+++ src/routes/jobs.ts
@@ -1,5 +1,8 @@
 import express from 'express'
 import type { Router } from 'express'
+import { getUpcommingJobs } from '../controllers/jobControllers'
 
 
 export const jobsRouter: Router = express.Router()
+
+jobsRouter.get('/upcomming', getUpcommingJobs)
