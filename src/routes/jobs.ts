4ef7b1aebd0426c88c026e340abd8633188796c1
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-12-05 02:56:39 +0100

    Add route for recent completed jobs in jobsRouter

--- src/routes/jobs.ts
+++ src/routes/jobs.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import type { Router } from 'express'
-import { getUpcommingJobs } from '../controllers/jobControllers'
+import { getCompletedJobs, getUpcommingJobs } from '../controllers/jobControllers'
 
 
 export const jobsRouter: Router = express.Router()
 
 jobsRouter.get('/upcomming', getUpcommingJobs)
+
+jobsRouter.get('/recent-completed', getCompletedJobs)
